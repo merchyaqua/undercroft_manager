70bf7e59dd5b67de61b405959b2102cf42552f70
merchyaqua <62640172+merchyaqua@users.noreply.github.com>
2025-01-28 21:50:41 +0000

    moved frontend files around

--- backend/sql/test_prop.sql
+++ backend/sql/test_prop.sql
@@ -1,15 +1,18 @@
--- INSERT INTO location (name) VALUES ('Undercroft') RETURNING locationID;
--- INSERT INTO category (name) VALUES ('Weapons') RETURNING categoryID;
+INSERT INTO location (name) VALUES ('Undercroft') RETURNING locationID;
+INSERT INTO category (name) VALUES ('Weapons') RETURNING categoryID;
 
--- INSERT INTO prop (name, description, categoryID, isBroken, locationID, photoPath)
--- VALUES (
---     'Sword',
---     'Great for breaking fingers',
---     (SELECT categoryID FROM category WHERE name = 'Weapons'),
---     FALSE,
---     (SELECT locationID FROM location WHERE name = 'Undercroft'),
---     'test photopath'
--- );
+INSERT INTO prop (name, description, categoryID, isBroken, locationID, photoPath)
+VALUES (
+    'Sword',
+    'Great for breaking fingers',
+    (SELECT categoryID FROM category WHERE name = 'Weapons'),
+    FALSE,
+    (SELECT locationID FROM location WHERE name = 'Undercroft'),
+    'test photopath'
+);
+
+INSERT INTO tag (name) VALUES ('Medieval');
+
 
 -- DELETE FROM prop WHERE propID = 2;
 
